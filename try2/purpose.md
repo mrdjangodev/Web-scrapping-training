93369b3cf4e2a724cc0296d944eb443372b49eb5
mrdjangodev <mirolimcoder@gmail.com>
2022-12-14 09:29:46 +0300

    Finding multiple tags and finding attributes done

---
+++ try2/purpose.md
@@ -0,0 +1,8 @@
+1. [X] Searching for tags
+2. [X] Tag arrtibutes
+3. [X] Find multiple tags
+4. [ ] Find attributes
+5. [ ] Find class names
+6. [ ] Find reqular expressions
+7. [ ] Find limits
+8. [ ] Save modified HTML
--- try2/try1.py
+++ try2/try1.py
@@ -3,8 +3,19 @@ from bs4 import BeautifulSoup
 with open("try2/index2.html", 'r') as f:
     doc = BeautifulSoup(f, 'html.parser')
 
-tag = doc.find("option")
-print(tag)
-tag['selected'] = True
-tag['value'] = "Changed value"
-print(tag)
+# # searching for tags
+# tag = doc.find("option")
+# print(tag)
+# tag['selected'] = True
+# tag['value'] = "Changed value"
+# print(tag)
+# print(tag.attrs) # finding tag attributes
+
+
+# finding multiple tags
+tags = doc.find_all(['p', 'div', 'li'])
+print(tags)
+
+# finding attributes
+tags = doc.find_all('option', text="Undergraduate")
+print(tags)
